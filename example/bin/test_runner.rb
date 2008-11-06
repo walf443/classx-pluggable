cd5ea112391bdc2962466c34b86d72e7a74511b6
Keiji, Yoshimi <walf443@gmail.com>
2008-11-07 08:20:28 +0900

    fixed example loading conf/config.yaml as default.

--- example/bin/test_runner.rb
+++ example/bin/test_runner.rb
@@ -3,7 +3,12 @@ $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib
 require 'test_runner'
 
 require 'yaml'
-config = YAML.load( ARGF )
+conf = ARGV[0] || File.join(File.dirname(__FILE__), '..', 'conf/config.yaml')
+
+config = nil
+File.open(conf) do |f|
+  config = YAML.load(f.read)
+end
 app = TestRunner.new(config["global"].merge({ :test_cases => [:foo, :bar, :baz]})) # dummy
 app.load_plugins(config["plugins"])
 
