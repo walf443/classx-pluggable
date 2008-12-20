888a46731400f5642059633173149fffd070854c
Keiji, Yoshimi <walf443@gmail.com>
2008-12-20 12:20:01 +0900

    create gh-pages branch.

--- spec/classx-pluggable_spec.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.dirname(__FILE__), 'spec_helper')
-require 'classx/pluggable'
-
-describe ClassX::Pluggable do
-end
--- spec/spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'spec'
-$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + '/../lib/'))
-
-require 'classx/pluggable'
-
