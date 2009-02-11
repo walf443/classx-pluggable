57e203af42f390341dd829f07151c7fff46d8ad8
Keiji, Yoshimi <walf443@gmail.com>
2009-02-11 11:58:09 +0900

    fix failed example test.

--- examples/test_runner/bin/test_runner.rb
+++ examples/test_runner/bin/test_runner.rb
@@ -4,7 +4,6 @@ require 'test_runner'
 
 require 'yaml'
 conf = ARGV[0] || File.join(File.dirname(__FILE__), '..', 'conf/config.yaml')
-
 config = nil
 File.open(conf) do |f|
   config = YAML.load(f.read)
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -1,17 +1,19 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 require 'logger'
 require 'stringio'
+require 'rake'
 
-def example_check name
+def example_check name, bind=binding
   base_dir = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
   target_runner = File.join(base_dir, 'examples', name, 'bin', "#{name}.rb")
 
   describe "example", name do
     it "should not raise Exception" do
+      # FIXME: bad hack. is there any better way?
       lambda {
-        $logger = Logger.new(StringIO.new)
-        load target_runner
+        include FileUtils
+        sh "ruby -rrubygems #{target_runner}"
       }.should_not raise_error(Exception)
     end
   end
